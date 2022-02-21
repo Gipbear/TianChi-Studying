6ff7377cff51b981213028a684abad156943d90e
gipbear <1099048889@qq.com>
2022-02-21 23:52:45 +0800

    day06位运算

---
+++ 天池在线编程/day06位运算/231.2-的幂.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=231 lang=cpp
+ *
+ * [231] 2 的幂
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        return (n & -n) == n;
+    }
+};
+// @lc code=end
+
